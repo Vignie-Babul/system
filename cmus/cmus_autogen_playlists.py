e0d803a4362fc9259b8b46e367a8a3b2a52fadbe
Vignie Babul <vigniebabul.dev@gmail.com>
2026-03-09 20:55:47 +0200

    Extracted const to the main function

--- cmus/cmus_autogen_playlists.py
+++ cmus/cmus_autogen_playlists.py
@@ -4,7 +4,7 @@ import os
 
 '''script ignores files, that start with PRIVATE (double underscore) `tag`'''
 
-PRIVATE = '__'
+PRIVATE_TAG = '__'
 
 HOMEDIR = os.path.expanduser('~')
 MUSIC_DIR = f'{HOMEDIR}/audio/music'
@@ -18,8 +18,8 @@ def is_dir_exists(path: str) -> bool:
 
 	return True
 
-def is_file_private(path: str) -> bool:
-	return os.path.basename(path).startswith(PRIVATE)
+def is_file_private(path: str, private_tag: str) -> bool:
+	return os.path.basename(path).startswith(private_tag)
 
 def get_subdirs_abs_paths(path: str) -> Iterator[str]:
 	for dir_ in sorted(os.listdir(path)):
@@ -30,7 +30,7 @@ def write_playlist(filepath: str, songs: list[str]) -> None:
 		print(*songs, sep='\n', file=playlist)
 		print(f"Created '{filepath}'")
 
-def main(music_dir: str, pl_dir: str) -> None:
+def main(music_dir: str, pl_dir: str, private_tag: str) -> None:
 	if not is_dir_exists(pl_dir) or not is_dir_exists(music_dir):
 		return
 
@@ -43,11 +43,11 @@ def main(music_dir: str, pl_dir: str) -> None:
 
 		songs = get_subdirs_abs_paths(music_abs_path)
 		only_public_file_songs = filter(
-			lambda x: os.path.isfile(x) and not is_file_private(x),
+			lambda x: os.path.isfile(x) and not is_file_private(x, private_tag),
 			songs,
 		)
 		write_playlist(pl_abs_path, only_public_file_songs)
 
 
 if __name__ == '__main__':
-	main(MUSIC_DIR, CMUS_PLAYLISTS_DIR)
+	main(MUSIC_DIR, CMUS_PLAYLISTS_DIR, PRIVATE_TAG)
